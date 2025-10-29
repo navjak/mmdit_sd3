3141544950e9d8e59dda05addf905ba5e1580753
navjak <navdeepjakhar30@gmail.com>
2025-10-29 18:19:29 +0530

    added norm code

--- mmdit_pytorch/normalization.py
+++ mmdit_pytorch/normalization.py
@@ -3,7 +3,7 @@ from torch import nn
 
 
 # MultiHead RMSNorm
-class MultiHeadRMSNorm(Module):
+class MultiHeadRMSNorm(nn.Module):
     def __init__(self, dim, heads = 1):
         super().__init__()
         self.scale = dim ** 0.5 # for scaling the output
@@ -11,13 +11,35 @@ class MultiHeadRMSNorm(Module):
 
     def forward(self, x):
         return F.normalize(x, dim = -1) * self.gamma * self.scale
-
-
-
-# LayerNorm
-
-
-
-
+    
 
 # adaLN
+class AdaLN(nn.Module):
+    def __init__(self, dim, cond_dim, num_modulations=1, eps=1e-6):
+        """
+        - cond_dim: conditioning vector dimension (pooled text + timestep embedding)
+        - num_modulations: number of (gamma, beta) pairs required (set to 1 for default; set > 1 if several modulations from one cond vector)
+        """
+        super().__init__()
+        self.norm = nn.LayerNorm(dim, elementwise_affine=False, eps=eps)
+        self.modulation = nn.Linear(cond_dim, num_modulations * dim * 2)
+        nn.init.zeros_(self.modulation.weight)
+        nn.init.zeros_(self.modulation.bias)
+        self.num_modulations = num_modulations
+
+    def forward(self, x, cond, modulation_index=0):
+        """
+        - x: [B, N, dim] or [B, dim]
+        - cond: [B, cond_dim]
+        - modulation_index: which (gamma, beta) pair to use (0 by default)
+        """
+        x_norm = self.norm(x)
+        mod_params = self.modulation(cond)  # [batch_size, num_modulations * dim * 2]
+        params = mod_params.view(x.shape[0], self.num_modulations, 2, x_norm.shape[-1])
+        gamma = params[:, modulation_index, 0, :]  # [batch_size, dim]
+        beta = params[:, modulation_index, 1, :]   # [batch_size, dim]
+        # broadcast
+        while gamma.dim() < x_norm.dim():
+            gamma = gamma.unsqueeze(1)
+            beta = beta.unsqueeze(1)
+        return gamma * x_norm + beta
